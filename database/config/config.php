e64abd4e31e7f1d20bdb6622abdaf69b241d20d8
Nilesh Vishwakarma <nileshv@axioned.com>
2022-05-21 16:10:28 +0530

    change database username and password

--- database/config/config.php
+++ database/config/config.php
@@ -3,8 +3,8 @@
 require_once('DatabaseConnection.php');
 
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
+define('DB_USER', 'phpmyadmin');
+define('DB_PASSWORD', 'root');
 define('DB_DATABASE', 'multiauth_system');
 
 $db = new DatabaseConnection;
